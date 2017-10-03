5b2df93e76900c82db005705cbce70d42d94cfed
Oliver Andrews <unkn0wnvariable@users.noreply.github.com>
2017-10-03 16:41:06 +0100

    Added comments

--- Active Directory/Info Gathering/AD Operating Systems.ps1
+++ Active Directory/Info Gathering/AD Operating Systems.ps1
@@ -1,8 +1,13 @@
 ﻿# List operating systems in AD and how many machines are running each one.
+#
 
+# Get all AD computers and include the OperatingSystem property
 $adComputers = Get-ADComputer -Filter * -Properties OperatingSystem
+
+# Gather up all unique OperatingSystems from the AD computers into one list
 $operatingSystems = $adComputers.OperatingSystem | Sort-Object -Unique
 
+# Output to screen each operating system and how many AD computers are running it
 ForEach ($operatingSystem in $operatingSystems) {
     $computers = ($adComputers | Where-Object {$_.OperatingSystem -eq $operatingSystem}).Name
     Write-Host ([string]$computers.Count + ' running ' + $operatingSystem + '.')
