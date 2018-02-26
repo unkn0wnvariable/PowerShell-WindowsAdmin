1e6cc42ba3db16f419372be6ccb0e32c57c7b1e0
Oliver Andrews <unkn0wnvariable@users.noreply.github.com>
2018-02-26 15:57:31 +0000

    Tweaking

--- VMware/Detach iSCSI LUNs (naa).ps1
+++ VMware/Detach iSCSI LUNs (naa).ps1
@@ -21,14 +21,25 @@ Foreach($vmHost in $vmHosts)
     # Iterate through the datastores to be removed...
     Foreach($datastoreNaa in $datastoreNaas)
     {
-        # Use the ID from above to get the UUID of the iSCSI LUN
-        $lunUuid = (Get-ScsiLun -VmHost $vmHost -CanonicalName $datastoreNaa).ExtensionData.Uuid
-
         # What's going on?
-        Write-Host "Detaching $lunUuid from $vmHost."
+        Write-Host "Detaching LUN $datastoreNaa from $vmHost... " -NoNewline
 
-        # Open a connection to the VMware Storage System and detach the LUN from the host using the UUID
-        $vmStorage = Get-View $vmHost.Extensiondata.ConfigManager.StorageSystem
-        $vmStorage.DetachScsiLun($lunUuid)
+        # Use the ID from above to get the UUID of the iSCSI LUN
+        $lunUuid = $null
+        $lunUuid = (Get-ScsiLun -VmHost $vmHost -CanonicalName $datastoreNaa -ErrorAction SilentlyContinue).ExtensionData.Uuid
+
+        If ($lunUuid -ne $null){
+            # Open a connection to the VMware Storage System and detach the LUN from the host using the UUID
+            $vmStorage = Get-View $vmHost.Extensiondata.ConfigManager.StorageSystem
+            $vmStorage.DetachScsiLun($lunUuid)
+
+            Write-Host "Completed." -ForegroundColor Green
+        }
+        Else {
+            Write-Host "Not attached." -ForegroundColor Red
+        }
     }
 }
+
+# Disconnect from the vSphere server
+Disconnect-VIServer -Server $viServer
