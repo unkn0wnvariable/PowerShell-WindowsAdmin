dbd49cae974eddcafafc820ae6b3902a7d79d246
Oliver Andrews <unkn0wnvariable@users.noreply.github.com>
2018-03-06 16:06:37 +0000

    Added credentials use

--- VMware/Start SSH On All Hosts.ps1
+++ VMware/Start SSH On All Hosts.ps1
@@ -4,11 +4,12 @@
 # Initialise PowerCLI Environment
 .'C:\Program Files (x86)\VMware\Infrastructure\PowerCLI\Scripts\Initialize-PowerCLIEnvironment.ps1'
 
-# Get- VIServer
-$viServer = Read-Host -Prompt 'Enter the FQDN of your VI server'
+#Get Admin Credentials
+$adminCreds = Get-Credential -Message 'Enter account details with admin rights to VMware'
 
-# Connect vCenter
-Connect-VIServer -Server $viServer
+# Connect to the vSphere server
+$viServer = Read-Host -Prompt 'Enter hostname of vSphere server'
+Connect-VIServer -Server $viServer -Credential $adminCreds
 
 # Start SSH on hosts
 Get-VMHost -Server $viServer | ForEach {Start-VMHostService -HostService ($_ | Get-VMHostService | Where {$_.Key -eq “TSM-SSH”})}
