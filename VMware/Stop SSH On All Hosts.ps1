bc9dfeaed34ba9616e1c8166c2b52d9599fad6db
Oliver Andrews <unkn0wnvariable@users.noreply.github.com>
2018-03-09 12:07:42 +0000

    Updated for PCLI 10

--- VMware/Stop SSH On All Hosts.ps1
+++ VMware/Stop SSH On All Hosts.ps1
@@ -1,14 +1,24 @@
 ﻿# Script to Enable SSH on all hosts
+#
+# Updated for PowerCLI 10
+#
+# If you're using self signed certificates you will need to revert the invalid certificate
+# behaviour to warn instead of stop, as was the case in PowerCLI 6.5
+#
+# This is done using the following command:
+#
+# Set-PowerCLIConfiguration -InvalidCertificateAction Warn -Confirm:$false
+#
 
+# Import the PowerCLI Module
+Import-Module -Name VMware.PowerCLI
 
-# Initialise PowerCLI Environment
-.'C:\Program Files (x86)\VMware\Infrastructure\PowerCLI\Scripts\Initialize-PowerCLIEnvironment.ps1'
+#Get Admin Credentials
+$adminCreds = Get-Credential -Message 'Enter account details with admin rights to VMware'
 
-# Get- VIServer
-$viServer = Read-Host -Prompt 'Enter the FQDN of your VI server'
-
-# Connect vCenter
-Connect-VIServer -Server $viServer
+# Connect to the vSphere server
+$viServer = Read-Host -Prompt 'Enter hostname of vSphere server'
+Connect-VIServer -Server $viServer -Credential $adminCreds
 
 # Stop SSH on hosts
 Get-VMHost -Server $viServer | ForEach {Stop-VMHostService -HostService ($_ | Get-VMHostService | Where {$_.Key -eq “TSM-SSH”}) -Confirm:$false}
