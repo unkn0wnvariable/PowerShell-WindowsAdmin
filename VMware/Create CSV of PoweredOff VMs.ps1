79fce9a82c09389d687156a82e7b815f08fde803
Oliver Andrews <unkn0wnvariable@users.noreply.github.com>
2018-03-15 16:15:05 +0000

    New scripts

---
+++ VMware/Create CSV of PoweredOff VMs.ps1
@@ -0,0 +1,40 @@
+﻿# Output a CSV file containing some details about powered off VMs
+#
+# Created for PowerCLI 10
+#
+
+# Import the PowerCLI Module
+Import-Module -Name VMware.PowerCLI -Force
+
+#Get Credentials
+$viCredential = Get-Credential -Message 'Enter credentials for VMware connection'
+
+# Connect to the vSphere server
+$viServer = Read-Host -Prompt 'Enter hostname of vSphere server'
+Connect-VIServer -Server $viServer -Credential $viCredential
+
+# Path to create output file
+$outputFile = 'C:\Temp\PoweredOffVMs.csv'
+
+# Get list of all powered off VMs
+$vmList = Get-VM -Server $viServer | Where-Object {$_.PowerState -eq 'PoweredOff'}
+
+# Create blank hashtable for results
+$outputTable = @()
+
+# Run through the VM's getting the information we need
+ForEach ($vm in $vmList) {
+    $datastoreName = (Get-View $vm.DatastoreIdList).Name
+    $vmProvisionedSpace = [math]::Round($vm.ProvisionedSpaceGB,2)
+    $vmFolder = $vm.Folder
+    $vmNotes = $vm.Notes
+    $vmResourcePool = $vm.ResourcePool
+    $outputLine = [pscustomobject]@{'VM Name'=$vm.Name;'Size (GB)'=$vmProvisionedSpace;'Folder'=$vmFolder;'Datastore'=$datastoreName;'Resource Pool'=$vmResourcePool;'Notes'=$vmNotes}
+    $outputTable += $outputLine
+}
+
+# Output the collected info to a file
+$outputTable | Export-Csv -Path $outputFile -NoTypeInformation
+
+# Disconnect from the vSphere server
+Disconnect-VIServer -Server $viServer -Confirm:$false
---
+++ VMware/Get VM Size and Current Datastore to CSV.ps1
@@ -0,0 +1,47 @@
+﻿# Get the size and current datastore location for a list of VMs
+#
+# Created for PowerCLI 10
+#
+
+# Import the PowerCLI Module
+Import-Module -Name VMware.PowerCLI -Force
+
+#Get Credentials
+$viCredential = Get-Credential -Message 'Enter credentials for VMware connection'
+
+# Connect to the vSphere server
+$viServer = Read-Host -Prompt 'Enter hostname of vSphere server'
+Connect-VIServer -Server $viServer -Credential $viCredential
+
+# Path to file containing list of VM's to shutdown
+$vmListFile = 'C:\Temp\VMList.txt'
+
+# Path to create output file
+$outputFile = 'C:\Temp\VMList.csv'
+
+# Get list of VMs from file and remove any duplicates
+$vmList = Get-Content -Path $vmListFile | Sort-Object -Unique
+
+# Create blank hashtable for results
+$outputTable = @()
+
+# Run through the VM's getting the information we need
+ForEach ($VM in $vmList) {
+    Write-Host ('Getting Details for ' + $VM)
+    Try {
+        $vmDetails = Get-VM -Name $VM -Server $viServer -ErrorAction Stop
+        $datastoreName = (Get-View $vmDetails.DatastoreIdList).Name
+        $vmProvisionedSpace = [math]::Round($vmDetails.ProvisionedSpaceGB,2)
+        $outputLine = [pscustomobject]@{'VM Name'=$VM;'Size (GB)'=$vmProvisionedSpace;'Current Datastore'=$datastoreName}
+    }
+    Catch {
+        $outputLine = [pscustomobject]@{'VM Name'=$VM;'Size (GB)'='0';'Current Datastore'='Deleted'}
+    }
+    $outputTable += $outputLine
+}
+
+# Output the collected info to a file
+$outputTable | Export-Csv -Path $outputFile -NoTypeInformation
+
+# Disconnect from the vSphere server
+Disconnect-VIServer -Server $viServer -Confirm:$false
