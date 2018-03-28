955c3a37265990986d4501c49493cdcb1e8a8460
Oliver Andrews <unkn0wnvariable@users.noreply.github.com>
2018-03-28 10:48:42 +0100

    New script

---
+++ VMware/Move a List of VMs.ps1
@@ -0,0 +1,31 @@
+﻿# Bulk Move VMs to Different Datastores Based on CSV Input
+#
+# Input CSV file should be in the format of 2 columns headed Name and TargetDatastore
+# with each row containing the VM name and the datastore to which it is to be moved.
+#
+# Updated for PowerCLI 10
+#
+
+# Import the PowerCLI Module
+Import-Module -Name VMware.PowerCLI -Force
+
+#Get Credentials
+$viCredential = Get-Credential -Message 'Enter credentials for VMware connection'
+
+# Connect to the vSphere server
+$viServer = Read-Host -Prompt 'Enter hostname of vSphere server'
+Connect-VIServer -Server $viServer -Credential $viCredential
+
+# Path to file containing list of VM's to move
+$vmsToMoveFile = 'C:\Temp\VMsToMoveStorage.csv'
+
+# Get list of VMs to be deleted from file
+$vmsToMove = Import-Csv -Path $vmsToMoveFile
+
+# Move each VM to its target datastore
+ForEach ($VM in $vmsToMove) {
+    Move-VM -VM $VM.Name -Server $viServer -Datastore $VM.TargetDatastore
+}
+
+# Disconnect from the vSphere server
+Disconnect-VIServer -Server $viServer -Confirm:$false
