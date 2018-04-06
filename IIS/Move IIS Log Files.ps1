0e2fcba2d3b22b26b49136183768c662b3d0e2fa
Oliver Andrews <unkn0wnvariable@users.noreply.github.com>
2018-04-06 12:01:27 +0100

    Added comments

--- IIS/Move IIS Log Files.ps1
+++ IIS/Move IIS Log Files.ps1
@@ -1,17 +1,30 @@
+# Script to move the IIS log files to a new location.
+#
+# There is, or at least was, no easy way to do this from the IIS GUI
+#
+#
 
+# Where do we want the logs to be?
 $logFileDirectory = 'I:\Logs\LogFiles'
 $failedRequestsLoggingDirectory = 'I:\Logs\FailedReqLogFiles'
 
+# Where are they now?
 $oldLogFilesDirectory = 'C:\inetpub\logs'
 
+# Import the IIS module
 Import-Module WebAdministration
 
+# Get all the sites served by IIS
 $sites = Get-ChildItem -Path iis:\sites\*
 
+# Update the log file location for each site
 ForEach($site in $sites) {
     Set-ItemProperty IIS:\Sites\$($site.Name) -name logFile.directory -value $logFileDirectory
     Set-ItemProperty IIS:\Sites\$($site.Name) -name traceFailedRequestsLogging.directory -value $failedRequestsLoggingDirectory
 }
 
+# Restart IIS gracefully
 IISReset
+
+# Remove the old log files
 Remove-Item $oldLogFilesDirectory -recurse
