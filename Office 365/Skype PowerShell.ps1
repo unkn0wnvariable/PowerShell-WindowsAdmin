e158d198f60f9193ed2233d355b1ba559d8d1173
Oliver Andrews <unkn0wnvariable@users.noreply.github.com>
2018-04-12 13:48:27 +0100

    Files deleted

--- Office 365/Skype PowerShell.ps1
+++
@@ -1,5 +0,0 @@
-﻿# Establish a session with Skype for Business online PowerShell
-
-$credential = Get-Credential
-$session = New-CsOnlineSession -Credential $credential -Verbose
-Import-PSSession $session
