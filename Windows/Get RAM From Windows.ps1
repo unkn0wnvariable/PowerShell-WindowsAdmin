173a36b835aa9bfc16a24720c1d7932883d776eb
Oliver Andrews <unkn0wnvariable@users.noreply.github.com>
2018-04-13 16:09:22 +0100

    New script

---
+++ Windows/Get RAM From Windows.ps1
@@ -0,0 +1,22 @@
+﻿# Get Memory Information from Remote Computer
+#
+
+# What computer are we accessing?
+$ComputerName = Read-Host -Prompt 'Enter computer to get information from'
+
+# Use WMI to get the information
+$PhysicalMemoryArray = @(Get-WmiObject -Class 'win32_PhysicalMemoryArray' -namespace 'root\CIMV2' -ComputerName $ComputerName)
+$PhysicalMemory = @(Get-WmiObject -Class 'win32_PhysicalMemory' -namespace 'root\CIMV2' -ComputerName $ComputerName)
+
+# How many memory arrays are there?
+Write-Output -InputObject ('Computer has ' + $PhysicalMemoryArray.Count + ' memory array(s).')
+
+# How many slots does each array have?
+Foreach ($MemoryArray In $PhysicalMemoryArray){
+     Write-Output -InputObject ('Total DIMM Slots in ' + $MemoryArray.Tag + ': ' + $MemoryArray.MemoryDevices)
+}
+
+# What's installed in the arrays?
+Foreach ($MemoryObject In $PhysicalMemory) {
+     Write-Output -InputObject ('DIMM installed in ' + $MemoryObject.DeviceLocator + ': ' + ($MemoryObject.Capacity / 1GB) + 'GB')
+}
