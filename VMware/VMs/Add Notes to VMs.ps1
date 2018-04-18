09ad52c7db91040e8c9c38c0b6588d53d2406ca2
Oliver Andrews <unkn0wnvariable@users.noreply.github.com>
2018-04-18 13:39:39 +0100

    New script

---
+++ VMware/VMs/Add Notes to VMs.ps1
@@ -0,0 +1,44 @@
+﻿# Add Notes to VMs
+# 
+# Written for PowerCLI 10
+#
+
+<#
+Please note:
+
+This script uses a CSV file as input and expects that file to have 5 columns, which are
+expected to be vmname, service, product, owner and description.
+
+There is no reason for this other than it just happens to be the information that we store
+in the VM notes - feel free to change it to suit your own needs.
+#>
+
+# Get Credentials
+$viCredential = Get-Credential -Message 'Enter credentials for VMware connection'
+
+# Get vSphere server name
+$viServer = Read-Host -Prompt 'Enter hostname of vSphere server'
+
+# Import the PowerCLI Module and Connect
+Import-Module -Name VMware.PowerCLI -Force
+Connect-VIServer -Server $viServer -Credential $viCredential
+
+# The CSV file containing all the info.
+$inputFile = 'C:\Temp\vm-notes.csv'
+
+# Each line of notes to add.
+$allVMs = Import-Csv -Path $inputFile
+
+# For each line of the CSV file, compile the notes and add them to the VM.
+foreach ($vm in $allVMs) {
+    $notes = @(
+        ('Service: ' + $vm.service),
+        ('Product: ' + $vm.product),
+        ('Owner: ' + $vm.owner),
+        ('Description: ' + $vm.description)
+    ) -join "`n"
+    Get-VM -Server $viServer -Name $vm.vmname | Set-VM -Notes $notes -Confirm:$false
+}
+
+# Disconnect from the vSphere server
+Disconnect-VIServer -Server $viServer -Confirm:$false
