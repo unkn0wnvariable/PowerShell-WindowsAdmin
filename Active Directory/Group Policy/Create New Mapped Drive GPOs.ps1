b3ef58177424c273a04fb6d6cbe01b5855bc720a
Oliver Andrews <unkn0wnvariable@users.noreply.github.com>
2018-04-20 11:28:43 +0100

    Code reformatting

--- Active Directory/Group Policy/Create New Mapped Drive GPOs.ps1
+++ Active Directory/Group Policy/Create New Mapped Drive GPOs.ps1
@@ -8,14 +8,16 @@ $inputFile = 'C:\Temp\New-GPO-Mapped-Drives.csv'
 $existingMappedDrives = Import-Csv -Path $inputFile
 
 # Test paths and output a list of those which are not valid.
-foreach ($existingMappedDrive in $existingMappedDrives) {
-    #If (!(Test-Path -Path $existingMappedDrive.MappedPath)) {
-    #    Write-Host ($existingMappedDrive.MappedPath + ' referenced in ' + $existingMappedDrive.GPOName + ' is not online.')
-    #}
-    $existingMappedDrive
-    if ($existingMappedDrive.Exists) {
+foreach ($existingMappedDrive in $existingMappedDrives)
+{
+    if (!(Test-Path -Path $existingMappedDrive.MappedPath))
+    {
+        Write-Output ($existingMappedDrive.MappedPath + ' referenced in ' + $existingMappedDrive.GPOName + ' is not online.')
+    }
+    if ($existingMappedDrive.Exists)
+    {
         $shareServer = [regex]::match($existingMappedDrive.MappedPath,'[^\\]+').value
         $mappedDrive = $existingMappedDrive.DriveLetter.ToLower()
-        Write-Host ($existingMappedDrive.MappedPath + ' on server ' + $shareServer + ' to drive ' + $mappedDrive + ' referenced in ' + $existingMappedDrive.GPOName + '.')
+        Write-Output ($existingMappedDrive.MappedPath + ' on server ' + $shareServer + ' to drive ' + $mappedDrive + ' referenced in ' + $existingMappedDrive.GPOName + '.')
     }
 }
