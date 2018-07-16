c2e1004a86a31903f1c6ec9c736b07df83d59fe5
Oliver Andrews <unkn0wnvariable@users.noreply.github.com>
2018-07-16 14:59:45 +0100

    Corrected case

--- WindowsServer/Remove Options from DHCP.ps1
+++ WindowsServer/Remove Options from DHCP.ps1
@@ -6,14 +6,16 @@
 # Import the DHCP Server module
 Import-Module DhcpServer
 
-# Create array containing all DHCP servers to have options removed
+
+# Which servers to remove options from?
 $dhcpServers = @('','')
 
-# Create array containing options to be removed
+# Which options to remove?
 $optionsToRemove = @('','')
 
+
 # Iterate through the DHCP servers
-ForEach ($dhcpServer in $dhcpServers)
+foreach ($dhcpServer in $dhcpServers)
 {
     # Get existing server options
     $serverOptions = Get-DhcpServerv4OptionValue -ComputerName $dhcpServer
@@ -22,23 +24,23 @@ ForEach ($dhcpServer in $dhcpServers)
     $dhcpScopes = Get-DhcpServerv4Scope -ComputerName $dhcpServer
 
     # Iterate through options to be removed
-    ForEach ($optionToRemove in $optionsToRemove)
+    foreach ($optionToRemove in $optionsToRemove)
     {
         # If the option is in the server options, then remove it.
-        If ($optionToRemove -in $serverOptions.OptionId)
+        if ($optionToRemove -in $serverOptions.OptionId)
         {
             Write-Output -InputObject ('Removing server option ' + $optionToRemove + ' from ' + $dhcpServer + '.'  )
             Remove-DhcpServerv4OptionValue -ComputerName $dhcpServer -OptionId $optionToRemove
         }
 
         # Iterate through the DHCP scopes
-        ForEach ($dhcpScope in $dhcpScopes)
+        foreach ($dhcpScope in $dhcpScopes)
         {
             # Get options set up on scope
             $scopeOptions = Get-DhcpServerv4OptionValue -ComputerName $dhcpServer -ScopeId $dhcpScope.ScopeId
 
             # If the option to be removed is present, then remove it
-            If ($optionToRemove -in $scopeOptions.OptionId)
+            if ($optionToRemove -in $scopeOptions.OptionId)
             {
                 Write-Output -InputObject ('Removing scope option ' + $optionToRemove + ' from ' +  $dhcpScope.ScopeId + ' on ' + $dhcpServer + '.'  )
                 Remove-DhcpServerv4OptionValue -ComputerName $dhcpServer -ScopeId $dhcpScope.ScopeId -OptionId $optionToRemove
