9cbe2d91e5cd30b261b207cdc6afbf2e3a654b35
Oliver Andrews <unkn0wnvariable@users.noreply.github.com>
2018-08-17 16:51:55 +0100

    New script

---
+++ ActiveDirectory/InfoGathering/FindUsersWithMismatchedEmailAndUPN.ps1
@@ -0,0 +1,26 @@
+﻿# Find User accounts where UPN doesn't match the primary email address
+#
+
+## WORK IN PROGRESS ##
+
+# Import ActiveDirectory module
+Import-Module ActiveDirectory
+
+# Get admin credentials for Active Directory
+if ($null -eq $adminCredentials) {$adminCredentials = Get-Credential}
+
+# Get a list of possible UPN suffixes from the forest
+$upnSuffixes = (Get-ADForest).Domains
+$upnSuffixes += (Get-ADForest).UPNSuffixes
+
+$users = Get-ADUser -Filter '(enabled -eq $true) -and (EmailAddress -like "*")' -Properties EmailAddress,userPrincipalName,SamAccountName,enabled -Credential $adminCredentials
+
+$output = @()
+
+Foreach ($user in $users) {
+    If (($user.EmailAddress.Split('@')[1] -in $upnSuffixes) -and ($user.SamAccountName -notmatch $user.EmailAddress.Split('@')[0])) {
+    $output += $user.Name + ',' + $user.SamAccountName + ',' + $user.EmailAddress
+    }
+}
+
+$output | Format-Table
