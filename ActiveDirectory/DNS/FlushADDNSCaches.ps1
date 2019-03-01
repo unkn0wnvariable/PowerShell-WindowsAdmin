2f4302dd874d24e755d03dd941599b51026afd83
Oliver Andrews <unkn0wnvariable@users.noreply.github.com>
2019-03-01 15:39:08 +0000

    Update FlushADDNSCaches.ps1

--- ActiveDirectory/DNS/FlushADDNSCaches.ps1
+++ ActiveDirectory/DNS/FlushADDNSCaches.ps1
@@ -2,4 +2,4 @@
 #
 # Cmdlets used require the ADDS component from RSAT to be installed.
 
-Get-ADDomainController -Filter * | ForEach-Object { Clear-DnsServerCache –ComputerName $_.HostName -Force }
+Get-ADDomainController -Filter * | ForEach-Object { Clear-DnsServerCache -ComputerName $_.HostName -Force }
