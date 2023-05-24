42dc2abec27b40b3477823008575f8c0c0a2bf11
Oliver Andrews <3819627+unkn0wnvariable@users.noreply.github.com>
2023-05-24 09:34:50 +0100

    Update ADLogonOlderThan.ps1

--- ActiveDirectory/InfoGathering/ADLogonOlderThan.ps1
+++ ActiveDirectory/InfoGathering/ADLogonOlderThan.ps1
@@ -1,20 +1,37 @@
-﻿Import-Module ActiveDirectory
+﻿# Get all AD user accounts with a last logon date older than a cut-off we specify in days
+#
 
-$cutOffDate = (Get-Date).AddDays('-21')
+# Import the Active Directory module - requires AD RSAT tools to be installed
+Import-Module ActiveDirectory
 
-$outputFile = 'C:\Temp\OldLogons.csv'
-Out-File $outputFile -Encoding utf8 -Force
+# How long ago should we report on for the last login?
+$cutOffDate = (Get-Date).AddDays('-60')
 
-$tableHeader = 'Name,SamAccountName,UserPrincipalName,EmailAddress,lastLogon'
-Write-Output $tableHeader | Out-File $outputFile -Encoding utf8 -Append
+# Where should we output the results to?
+$outputFile = 'C:\Temp\LastLoginNoneAndStale.csv'
 
-$users = Get-ADUser -Filter {Enabled -eq 'True'} -Properties Name, SamAccountName, UserPrincipalName, EmailAddress, lastLogon
+# Get server and credentials for a domain controller to connect to
+$domainServer = Read-Host -Prompt 'Enter the FQDN of a domain controller'
+$domainCredential = Get-Credential -Message 'Enter credentials for an account with the necessary permissions'
 
-foreach ($user in $users) {
+# Get all users from AD
+$allUsers = Get-ADUser -Filter { Enabled -eq 'True' } -Properties Name, SamAccountName, UserPrincipalName, EmailAddress, lastLogon -Server $domainServer -Credential $domainCredential
+
+# Find all users with a last logon date older than the cut off set above
+$outputDetails = @()
+foreach ($user in $allUsers) {
     $userLastLogon = [datetime]::FromFileTimeUtc($user.lastLogon)
     if ($userLastLogon -lt $cutOffDate) {
-        if ($userLastLogon -eq '01/01/1601 00:00:00') {$userLastLogon = 'Never'}
-        $output = $user.Name + ',' + $user.SamAccountName + ',' + $user.UserPrincipalName + ',' + $user.EmailAddress + ',' + $userLastLogon.ToString()
-        Write-Output $output | Out-File $outputFile -Encoding utf8 -Append
+        if ($userLastLogon -eq '01/01/1601 00:00:00') { $userLastLogon = 'Never' } # replaces the default date for no-login with a more obvious 'Never'
+        $outputDetails += [PSCustomObject]@{
+            'Name'              = $user.Name;
+            'SamAccountName'    = $user.SamAccountName;
+            'UserPrincipalName' = $user.UserPrincipalName;
+            'EmailAddress'      = $user.EmailAddress;
+            'lastLogon'         = $userLastLogon.ToString();
+        }
     }
 }
+
+# Export results to CSV file
+$outputDetails | Export-Csv -Path $outputFile
